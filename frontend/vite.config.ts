77bcbc91a4f6c153df4f79cc660c0389a16d89ab
ThijmenBilderbeek <thijmenbilderbeek@gmail.com>
2026-03-18 16:20:44 +0100

    Update vite.config.ts with new configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import { resolve } from 'path'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,7 +14,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(import.meta.dirname, 'src')
+      '@': path.resolve(__dirname, 'src')
     }
   },
 });
